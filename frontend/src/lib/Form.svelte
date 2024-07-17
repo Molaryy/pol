173d8c90d1ff0633a8d7197fb05b2411d78aac8f
mohammed <molaryyios@gmail.com>
2024-07-17 23:54:15 +0200

    feat: improved frontend

--- frontend/src/lib/Form.svelte
+++ frontend/src/lib/Form.svelte
@@ -34,14 +34,20 @@
 
 <div class="form">
     <form on:submit|preventDefault={createPost}>
-        <input type="text" id="name" placeholder="Mohammed" bind:value={name}/>
-        <label for="name"/>
-        <input type="number" id="age" placeholder="19"  bind:value={age}/>
-        <label for="age"/>
-        <input type="text" id="email" placeholder="mohammed@test.com" bind:value={email}/>
-        <label for="email"/>
-        <input type="text" placeholder="Hello World!"  bind:value={description}/>
-        <label for="email"/>
+        <div class="form-inputs">
+            <label for="name">
+                <input type="text" id="name" placeholder="Mohammed" bind:value={name}/>
+            </label>
+            <label for="age">
+                <input type="number" id="age" placeholder="19"  bind:value={age}/>
+            </label>
+            <label for="email">
+                <input type="text" id="email" placeholder="mohammed@test.com" bind:value={email}/>
+            </label>
+            <label for="email">
+                <input type="text" id="description" placeholder="Hello World!"  bind:value={description}/>
+            </label>
+        </div>
         <button>
           Submit
         </button>
@@ -50,27 +56,41 @@
 
 <style>
     .form {
-        margin-left: 50%;
+        margin-top: 15rem;
+        margin-left: 45rem;
         width: 400px;
         height: 400px;
-        background-color: rgb(119, 209, 128);
+        background-color: rgb(196, 193, 188);
         border-radius: 15px;
-        display: flex;
+        box-shadow: 5px 0 5px 0;
     }
+
+    .form-inputs {
+        padding-top: 5rem;
+        margin-top: 5rem;
+        margin-left: 6rem;
+    }
+
     .form input {
-      border-radius: 5px;
+        justify-content: center;
+        margin-top: 5px;
+        border-radius: 5px;
 	    color: inherit;
 	    border: none;
 	    font: inherit;
 	    outline: inherit;
-      width: 200px;
-      height: 35px;
+        width: 200px;
+        height: 35px;
     }
     .form button {
-	    border: none;
-      background-color: red;
-      border-radius: 5px;
-      width: 80px;
-      height: 25px;
+        margin-top: 5rem;
+        margin-left: 9rem;
+        color: white;
+        border: none;
+        background-color: #151313;
+        border-radius: 5px;
+        width: 100px;
+        height: 30px;
+        cursor: pointer;
     }
 </style>
