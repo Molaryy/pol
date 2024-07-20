ec949798109ab745e30aa159b2e87cfc11842d9c
mohammed <molaryyios@gmail.com>
2024-07-20 02:04:26 +0200

    fix: ci

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,15 +1,25 @@
 describe('Post test', () => {
   it('Fills the form', () => {
-    cy.visit('http://192.168.1.126:5173/')
+    cy.visit('/')
+    
+    // name
     cy.get('#name').type('fake')
     cy.get('#name').should('have.value', 'fake')
+    
+    //age
     cy.get('#age').type('19')
     cy.get('#age').should('have.value', '19')
+    
+    // email
     cy.get('#email').type('fake@email.com')
     cy.get('#email').should('have.value', 'fake@email.com')
+    
+    // description
     cy.get('#description').type('I like donuts')
     cy.get('#description').should('have.value', 'I like donuts')
     cy.get("form").submit()
     cy.get("#post-button").click()
   })
+
+
 })
